aec8236b77bf600ca4d74a59f581f048ee12504c
FRIEDRICH M. SAM <churchillmerediths@gmail.com>
2022-10-26 15:03:54 +0000

    remove comma for config.json file

--- controllers/api/v1/incidents.js
+++ controllers/api/v1/incidents.js
@@ -1,6 +1,23 @@
 const axios = require('axios');
 
 module.exports.getIncidents = (request, response, next) => {
+    const weather_api_url = process.env.WEATEHER_API_URL;
+    let weather_result = {};
+
+    const config = {
+        method: 'get',
+        url: weather_api_url,
+        headers: { }
+    };
+
+    axios(config)
+        .then(result => {
+            console.log(JSON.stringify(result.data));
+        })
+        .catch(error => {
+            console.log('Error: ', error.message);
+        });
+
     response.status(200).json({status: true, message: "Successfully retrieved", data: {}});
 }
 
