893905ff61e152e399b70e3667cc51eae9844461
FRIEDRICH M. SAM <churchillmerediths@gmail.com>
2022-10-26 14:43:13 +0000

    remove comma for config.json file

--- app.js
+++ app.js
@@ -9,8 +9,6 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
-
-
 app.listen(PORT, () => {
    console.log("Server is running on PORT: %s", PORT);
 });
---
+++ routes/api.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('', null);
+
+router.post('', null);
+
+module.exports = router;
