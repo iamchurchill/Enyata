33f09b2397d2339c3daddfb65c8b0ce6c2561e32
FRIEDRICH M. SAM <churchillmerediths@gmail.com>
2022-10-27 16:43:00 +0000

    added ejs

---
+++ config/config.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+
+const {DB_USER, DB_PASSWORD, DB, DB_HOST, DB_PORT, DB_DIALECT} = process.env;
+
+module.exports = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT,
+    dialectOptions: {
+      bigNumberStrings: true
+    }
+  },
+  test: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT,
+    dialectOptions: {
+      bigNumberStrings: true
+    }
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT,
+    dialectOptions: {
+      bigNumberStrings: true,
+      ssl: {
+        //ca: fs.readFileSync(__dirname + '/mysql-ca-main.crt')
+      }
+    }
+  }
+};
