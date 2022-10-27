ab7cbd5214daf6cedde97542bfd0bb741500aafd
FRIEDRICH M. SAM <churchillmerediths@gmail.com>
2022-10-27 14:09:41 +0000

    added ejs

--- config/config.js
+++ config/config.js
@@ -4,34 +4,34 @@ const {DB_USER, DB_PASSWORD, DB, DB_HOST, DB_PORT, DB_DIALECT} = process.env;
 
 module.exports = {
     development: {
-        username: DB_USER,
-        password: DB_PASSWORD,
-        database: DB,
-        host: DB_HOST,
-        port: DB_PORT,
-        dialect: DB_DIALECT,
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: process.env.DB_DIALECT,
         dialectOptions: {
             bigNumberStrings: true
         }
     },
     test: {
-        username: process.env.CI_DB_USERNAME,
-        password: process.env.CI_DB_PASSWORD,
-        database: process.env.CI_DB_NAME,
-        host: '127.0.0.1',
-        port: 3306,
-        dialect: 'mysql',
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: process.env.DB_DIALECT,
         dialectOptions: {
             bigNumberStrings: true
         }
     },
     production: {
-        username: process.env.PROD_DB_USERNAME,
-        password: process.env.PROD_DB_PASSWORD,
-        database: process.env.PROD_DB_NAME,
-        host: process.env.PROD_DB_HOSTNAME,
-        port: process.env.PROD_DB_PORT,
-        dialect: 'mysql',
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: process.env.DB_DIALECT,
         dialectOptions: {
             bigNumberStrings: true,
             ssl: {
