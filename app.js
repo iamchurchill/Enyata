2f51c3cbae43b476475873a7b9cffb59b3e1fa18
FRIEDRICH M. SAM <churchillmerediths@gmail.com>
2022-10-28 04:18:43 +0000

    init

--- app.js
+++ app.js
@@ -1,28 +1,22 @@
 const express = require('express');
-const dotenv = require('dotenv');
 const cors = require('cors');
+const app = express()
+const dotenv = require('dotenv');
 dotenv.config();
 
-const apiRoutes =  require('./routes/api');
-const errorController = require('./controllers/api/v1/error');
-
 const PORT = process.env.PORT || 8888;
-const app = express();
 
 app.use(cors())
-
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
+const apiRoutes =  require('./routes/api');
+const errorController = require('./controllers/api/v1/error');
 app.use('/api/v1', apiRoutes);
 app.use(errorController.get404);
-
-app.use((error, request, response, next) => {
-   response.status(500).json({status: false, message: error.toString()});
-});
 
 app.listen(PORT, () => {
    console.log("Server is running on PORT: %s", PORT);
