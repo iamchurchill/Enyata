812a6022e78876a241c3bd639174bee710721649
FRIEDRICH M. SAM <churchillmerediths@gmail.com>
2022-10-28 12:21:56 +0000

    init

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
 const app = express()
 const dotenv = require('dotenv');
 dotenv.config();
 
 const PORT = process.env.PORT || 8888;
 
+app.use(helmet());
 app.use(cors())
+app.use(morgan('combined'));
+
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
