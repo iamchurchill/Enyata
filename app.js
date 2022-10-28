5684a2f8a05841b73ac163fc03b06cee532ec7a4
FRIEDRICH M. SAM <churchillmerediths@gmail.com>
2022-10-28 12:58:51 +0000

    init

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
+const fs = require('fs')
 const morgan = require('morgan');
+const path = require('path')
 const app = express()
 const dotenv = require('dotenv');
 dotenv.config();
@@ -12,17 +14,20 @@ app.use(helmet());
 app.use(cors())
 app.use(morgan('combined'));
 
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), {flags: 'a'})
+app.use(morgan('combined', {stream: accessLogStream}))
+
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const apiRoutes =  require('./routes/api');
+const apiRoutes = require('./routes/api');
 const errorController = require('./controllers/api/v1/error');
 app.use('/api/v1', apiRoutes);
 app.use(errorController.get404);
 
 app.listen(PORT, () => {
-   console.log("Server is running on PORT: %s", PORT);
+    console.log("Server is running on PORT: %s", PORT);
 });
