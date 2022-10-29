c01b1a7974dcd1bb665f698bc5ddd1571f67a657
FRIEDRICH M. SAM <churchillmerediths@gmail.com>
2022-10-29 04:38:02 +0000

    init

--- routes/api.js
+++ routes/api.js
@@ -11,9 +11,9 @@ router.get('/incidents', incidentController.getIncidents);
 router.post('/incident/store',
     [
         body('client_id', 'Client id is required').trim().notEmpty().withMessage('client id is required').bail().isNumeric().withMessage('client is should be an integer'),
-        body('incident_desc').trim().notEmpty().withMessage('incident description is required').isString().withMessage('string required'),
-        body('city').trim().notEmpty().withMessage('city is required').isString().withMessage('string required'),
-        body('country').trim().notEmpty().withMessage('country is required').isString().withMessage('string required'),
+        body('incident_desc').trim().notEmpty().withMessage('incident description is required').isString().withMessage('string is required for description'),
+        body('city').trim().notEmpty().withMessage('city is required').isString().withMessage('string is required for city'),
+        body('country').trim().notEmpty().withMessage('country is required').isString().withMessage('string is required for country'),
     ], incidentController.postIncident);
 
 module.exports = router;
