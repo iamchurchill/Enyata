c351608891f07fcabfe80d9747358f8caf3ed63b
FRIEDRICH M. SAM <churchillmerediths@gmail.com>
2022-10-29 08:50:53 +0000

    init

--- routes/api.js
+++ routes/api.js
@@ -12,7 +12,7 @@ router.post
 (
     '/incident/store',
     [
-        body('client_id', 'Client id is required').trim().notEmpty().withMessage('client id is required').bail().isNumeric().withMessage('client is should be an integer'),
+        body('client_id', 'Client id is required').trim().notEmpty().withMessage('client id is required').bail().isNumeric().withMessage('client id should be an integer'),
         body('incident_desc').trim().notEmpty().withMessage('incident description is required').isString().withMessage('string is required for description'),
         body('city').trim().notEmpty().withMessage('city is required').isString().withMessage('string is required for city'),
         body('country').trim().notEmpty().withMessage('country is required').isString().withMessage('string is required for country'),
