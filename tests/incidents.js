ab51278508a76bde7378077ae2bc1dd644c1a436
FRIEDRICH M. SAM <churchillmerediths@gmail.com>
2022-10-30 20:35:11 +0000

    Working on unit testing and moving of project dependencies

--- tests/incidents.js
+++ tests/incidents.js
@@ -1,27 +1,26 @@
 process.env.NODE_ENV = 'test';
 const {Incidents} = require('../models')
 
-let chai = require('chai');
-let chaiHttp = require('chai-http');
-let server = require('../app');
-let should = chai.should();
-
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const app = require('../app');
+const should = chai.should();
 
 chai.use(chaiHttp);
-//Our parent block
+
 describe('Incidents', () => {
-    beforeEach((done) => { //Before each test we empty the database
+    /*beforeEach((done) => { //Before each test we empty the database
         Incidents.destroy({}, (error) => {
             done();
         });
-    });
+    });*/
 
     /*
       * Test the /GET route
       */
     describe('/GET Incidents', () => {
         it('it should GET all the Incidents', (done) => {
-            chai.request(server)
+            chai.request(app)
                 .get('/api/v1/incidents')
                 .end((err, res) => {
                     res.should.have.status(200);
@@ -31,5 +30,4 @@ describe('Incidents', () => {
                 });
         });
     });
-
 });
